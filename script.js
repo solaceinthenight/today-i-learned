5329e528609c061ea5ee1998dbab7afe45ca2793
Jacob Lai <laijs1@uci.edu>
2023-05-20 14:25:27 -0700

    42. Objects.

--- script.js
+++ script.js
@@ -96,15 +96,34 @@ const str = `The current fact is "${text}". It is ${calcFactAge(
 console.log(str);
 */
 
-const fact = ["Lisbon is the capital of Portugal", 2015, true, "something"];
-console.log(fact);
-console.log(fact[0]);
-console.log(fact.length);
-console.log(fact[fact.length - 1]);
+// const fact = ["Lisbon is the capital of Portugal", 2015, true, "something"];
+// console.log(fact);
+// console.log(fact[0]);
+// console.log(fact.length);
+// console.log(fact[fact.length - 1]);
 
-const [text, createdIn, isCorrect] = fact;
-console.log(text);
-console.log(createdIn);
+// const [text, createdIn, isCorrect] = fact;
+// console.log(text);
+// console.log(createdIn);
 
-const newFact = [...fact, "society"]; // 3 dots (spread operator) unpack the original array
-console.log(newFact);
+// const newFact = [...fact, "society"]; // 3 dots (spread operator) unpack the original array
+// console.log(newFact);
+
+const factObj = {
+  text: "Lisbon is the capital of Portugal",
+  category: "society",
+  createdIn: 2015,
+  isCorrect: true,
+  createSummary: function () {
+    return `The fact "${
+      this.text
+    }" is from the category ${this.category.toUpperCase()}`;
+  },
+};
+
+console.log(factObj.text);
+console.log(factObj["text"]);
+
+const { category, isCorrect } = factObj;
+console.log(category);
+console.log(factObj.createSummary());
