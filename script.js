6dcfc1202c89347d1973aac8c1600c26be1e0902
Jacob Lai <laijs1@uci.edu>
2023-05-20 14:01:41 -0700

    37. Taking Decisions With if/else Statements.

--- script.js
+++ script.js
@@ -11,30 +11,56 @@ btn.addEventListener("click", function () {
   }
 });
 
-let votesInteresting = 23;
-let votesMindblowing = 5;
-const text = "Lisbon is the capital of Portugal";
+// let votesInteresting = 23;
+// let votesMindblowing = 5;
+// const text = "Lisbon is the capital of Portugal";
 
-votesInteresting = votesInteresting + 1;
-votesInteresting++;
-console.log(votesInteresting);
+// votesInteresting = votesInteresting + 1;
+// votesInteresting++;
+// console.log(votesInteresting);
 
-let totalUpvotes = votesInteresting + votesMindblowing;
-console.log("Upvotes:", totalUpvotes);
+// let totalUpvotes = votesInteresting + votesMindblowing;
+// console.log("Upvotes:", totalUpvotes);
 
-let votesFalse = 4;
-const isCorrect = votesFalse < totalUpvotes;
-const isEqual = votesFalse === totalUpvotes;
-console.log(isCorrect);
-console.log(isEqual);
+// let votesFalse = 4;
+// const isCorrect = votesFalse < totalUpvotes;
+// const isEqual = votesFalse === totalUpvotes;
+// console.log(isCorrect);
+// console.log(isEqual);
 
-console.log(parseInt("24.533ccc"));
+// console.log(parseInt("24.533ccc"));
 
 function calcFactAge(year) {
   const currentYear = new Date().getFullYear();
   const age = currentYear - year;
+
+  if (age >= 0) return age;
+  else return "Impossible year";
+
   return age;
 }
 
 const age1 = calcFactAge(2015);
 console.log(age1);
+console.log(calcFactAge(2020));
+console.log(calcFactAge(1990));
+console.log(calcFactAge(2037));
+
+let votesInteresting = 23;
+let votesMindblowing = 5;
+
+if (votesInteresting == votesMindblowing) {
+  alert("This fact is equally interesting and mindblowing");
+} else if (votesInteresting > votesMindblowing) {
+  console.log("Interesting fact!");
+} else if (votesInteresting < votesMindblowing) {
+  console.log("Mindblowing fact!!");
+}
+
+// falsy values: 0, '', null, undefined
+// truthy value: everything else...
+if (votesMindblowing) {
+  console.log("MINDBLOWING FACT!!!");
+} else {
+  console.log("Not so special...");
+}
