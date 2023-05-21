146d88b67234412983c3676694ff0f4fb9f77012
Jacob Lai <laijs1@uci.edu>
2023-05-21 14:08:03 -0700

    55. Rendering the List of Categories.

--- src/App.js
+++ src/App.js
@@ -78,7 +78,30 @@ function NewFactForm() {
 }
 
 function CategoryFilter() {
-  return <aside>Category filter</aside>;
+  return (
+    <aside>
+      <ul>
+        <li className="category">
+          <button
+            className="btn
+          btn-all-categories"
+          >
+            All
+          </button>
+        </li>
+        {CATEGORIES.map((cat) => (
+          <li key={cat.name} className="category">
+            <button
+              className="btn btn-category"
+              style={{ backgroundColor: cat.color }}
+            >
+              {cat.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 function FactList() {
